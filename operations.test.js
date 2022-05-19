38c5577f64e7faed00d3105204fec84cef0b3dc6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-19 22:34:01 +0300

    feat: add describe group tests assert

--- operations.test.js
+++ operations.test.js
@@ -1,37 +1,20 @@
-// const operations = require("./operations");
- 
-// it("should multiply two numbers", ()=>{
-//     const expectedResult = 15;
-//     const result = operations.multiply(3, 5);
-//     if(result!==expectedResult)throw new Error(`Expected ${expectedResult}, but got ${result}`);
-// });
-// it("should add two numbers", ()=>{
-//     const expectedResult = 16;
-//     const result = operations.add(9, 7);
-//     if(result!==expectedResult) throw new Error(`Expected ${expectedResult}, but got ${result}`);
-// });
- 
-// it("shoud async multiply two numbers", (done)=>{ 
-//     const expectedResult = 12;
-//     operations.multiplyAsync(4, 3, (result)=>{
-//         if(result!==expectedResult)throw new Error(`Expected ${expectedResult}, but got ${result}`);
-//         done();
-//     });
-// });
-
+// В метод describe() в качестве первого параметра передается описание тестов,
+//  а в качестве второго - функция, которая содержит тесты.
 
 const assert = require("assert");
 const operations = require("./operations");
  
-it("should multiply two numbers", ()=>{
-    const expected = 15;
-    const result = operations.multiply(3, 5);
-    assert.equal(result, expected);
-});
-
-// Для не равно notEqual выдаст ошибку
-it("should multiply two numbers", ()=>{
-    const expected = 15;
-    const result = operations.multiply(3, 5);
-    assert.notEqual(result, expected);
+describe("Operation Tests", function(){
+    it("should multiply two numbers", function(){
+         
+        const expected = 15;
+        const result = operations.multiply(3, 5);
+        assert.equal(result, expected);
+    });
+    it("should add two numbers", function(){
+         
+        const expected = 16;
+        const result = operations.add(9, 7);
+        assert.equal(result, expected);
+    });
 });
